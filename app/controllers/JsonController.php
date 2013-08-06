162ea57c6faeef66845da03bccf70a37d7531fd4
Riu <radoslawmuszynski@gmail.com>
2013-08-06 13:51:45 +0200

    modified:   app/controllers/JsonController.php

--- app/controllers/JsonController.php
+++ app/controllers/JsonController.php
@@ -16,7 +16,18 @@ class JsonController extends ControllerBase
 
 	public function createAction()
 	{
-		// create json's files
+		$langid = $this->session->get('langid');
+		$docs = $this->modelsManager->createBuilder()
+			->columns(array('p.ordinal','d.title', 'd.value', 'f.rst'))
+			->addFrom('PhaldocParts','p')
+			->join('PhaldocDocs', 'p.id = d.part_id','d','INNER')
+			->join('PhaldocFiles', 'p.file_id = f.id','f','INNER')
+			->where('d.lang_id = :lang:', array('lang' => $langid))
+			->andWhere('f.type != :type:', array('type' => '3'))
+			->orderBy('p.ordinal ASC')
+			->getQuery()->execute();
+			
+			
 		$this->response->redirect("json");
 	}
 
